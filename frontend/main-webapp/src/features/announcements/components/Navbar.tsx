9931780e5bddc41d4fe9c53daee878e70f1e871b
uyenpham139 <uyenpham47139@gmail.com>
2026-02-04 22:37:46 +0700

    fix(ux): modify filter dropdown for navbar

--- frontend/main-webapp/src/features/announcements/components/Navbar.tsx
+++ frontend/main-webapp/src/features/announcements/components/Navbar.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 
 import Dropdown from "../../../components/common/Dropdown/Dropdown";
+import { FilterButton } from "../../../components/common/Buttons/FilterButton";
+import { ContentList, type ContentItem } from "../../../components/common/Dropdown/ContentList";
 
 export function Navbar() {
 
@@ -9,13 +11,27 @@ export function Navbar() {
   const [activeMenu, setActiveMenu] = useState<string|null>(null)
 
   // Data
-  const filterContents = [
-    "ABC", "XYZ", "Điền", "sau", "nhé"
+  const filterContents: ContentItem[] = [
+    {
+      content: "ABC",
+      onClick: () => console.log("Filter Item 1")
+    },
+    {
+      content: "Filter 2",
+      onClick: () => console.log("Filter Item 2")
+    },
+    {
+      content: "Filter 3",
+      onClick: () => console.log("Filter Item 3")
+    },
   ]
 
+  // Functions
   const toggleMenu = (menuId: string) => {
     setActiveMenu(activeMenu === menuId ? null : menuId)
   }
+
+  const closeMenu = () => setActiveMenu(null)
 
   return(
     <nav className="navbar-style">
@@ -28,15 +44,15 @@ export function Navbar() {
 
 
       {/* Filter */}
-      {/* <Dropdown 
+      <Dropdown 
         isOpen={activeMenu === 'filter'}
         trigger={
-          < 
+          <FilterButton
             onClick={() => toggleMenu('filter')}
           />
         }
-        children={filterContents}
-      /> */}
+        children={<ContentList data={filterContents} emptyLabel="" onItemClick={closeMenu}/>}
+      />
       
     </nav>
   );
